e979d6ec7cecff79221319da2801240127cdf493
Sanaz <sanaz.jamloo@gmail.com>
2017-04-21 22:05:09 -0700

    Add destroy method

--- job-board/app/controllers/jobs_controller.rb
+++ job-board/app/controllers/jobs_controller.rb
@@ -21,6 +21,14 @@ class JobsController < ApplicationController
     @job.update_attributes(job_params)
     redirect_to jobs_path
   end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path
+end
+
+
 end
 
 private
